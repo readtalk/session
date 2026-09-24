34611c178100b12e4b0dc25a3610538ad036aff6
READTalk <redtalk.app@hotmail.com>
2026-09-25 00:32:50 +0700

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://session.readtalk.workers.dev/",
+	site: "https://readtalk.pages.dev/",
 	integrations: [mdx(), sitemap()],
 	adapter: cloudflare({
 		platformProxy: {
